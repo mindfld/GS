01c5b3146099f8bdcad493cd4efcd29738070239
sergiy.lesko <sergiy.lesko@intellectsoft.net>
2017-09-02 13:42:29 +0300

    changed return message

--- src/main/java/com/goldstardiana/gs/FondyCallbackController.java
+++ src/main/java/com/goldstardiana/gs/FondyCallbackController.java
@@ -4,15 +4,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @RestController
 public class FondyCallbackController {
     @RequestMapping("/zaza")
     public String index(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            sb.append(headerNames.nextElement()).append("\n");
+        }
 
-        return String.valueOf(request.getHeaderNames()) +
-                request.getAttributeNames() +
-                request.getParameterNames() +
-                request.getParameterMap();
+        Enumeration<String> attributeNames = request.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            sb.append(attributeNames.nextElement()).append("\n");
+        }
+
+        Enumeration<String> paramNames = request.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            sb.append(paramNames.nextElement()).append("\n");
+        }
+        sb.append(request.getParameterMap());
+        return sb.toString();
     }
 }
