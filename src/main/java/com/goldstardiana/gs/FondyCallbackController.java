29c642b78d4453a6506a134cc9025ec9b06fb8d0
sergiy.lesko <sergiy.lesko@intellectsoft.net>
2017-09-02 13:08:34 +0300

    1

--- src/main/java/com/goldstardiana/gs/FondyCallbackController.java
+++ src/main/java/com/goldstardiana/gs/FondyCallbackController.java
@@ -3,10 +3,17 @@ package com.goldstardiana.gs;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class FondyCallbackController {
     @RequestMapping("/zaza")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        System.out.println(request.getHeaderNames());
+        System.out.println(request.getAttributeNames());
+        System.out.println(request.getParameterNames());
+        System.out.println(request.getParameterMap());
+
         return "Greetings from Spring Boot!";
     }
 }
